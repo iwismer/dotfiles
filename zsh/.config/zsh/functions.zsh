f26ad5241835a9fc394cdb94935857c7b2ea9e7e
iwismer <isaac@iwismer.ca>
2020-11-20 15:14:25 -0500

    Add unarchive function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -26,16 +26,53 @@ function md_format {
 c() { printf "%s\n" "$*" | bc }
 alias c="noglob c" 
 
-#function rmi {
-#  docker images | tail -n +2 | while read -r line ; do
-#      echo "$line"
-#      read -q "var?Do you wish to remove this image?" < /dev/tty
-#      echo
-#      if [[ "$var" =~ ^[Yy]$ ]]
-#      then
-#          hash=`echo $line | awk '{print $3}'`
-#          docker rmi $hash
-#      fi
-#  done
-#}
-
+function unarchive() {
+	if [ -f $1 ]; then
+		case $1 in
+		*.tar.bz2)
+			tar xvjf $1
+			;;
+		*.tar.gz)
+			tar xvzf $1
+			;;
+		*.tar.xz)
+			tar xvf $1
+			;;
+		*.bz2)
+			bunzip2 $1
+			;;
+		*.rar)
+			unrar x $1
+			;;
+		*.gz)
+			gunzip $1
+			;;
+		*.tar)
+			tar xvf $1
+			;;
+		*.tbz2)
+			tar xvjf $1
+			;;
+		*.tgz)
+			tar xvzf $1
+			;;
+		*.zip)
+			unzip $1
+			;;
+		*.Z)
+			uncompress $1
+			;;
+		*.7z)
+			7z x $1
+			;;
+		*.apk)
+			unzip $1
+			;;
+		*)
+			echo "'$1' cannot be extracted via unarchive"
+			;;
+		esac
+	else
+		echo "'$1' is not a valid file"
+	fi
+}
