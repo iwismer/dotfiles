a9f1a9e62b86486b5669401be8061b5ec9f2418c
Isaac Wismer <isaac@iwismer.ca>
2021-01-29 20:06:26 -0500

    change config- alias to c-

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -32,12 +32,12 @@ alias backup='sudo rsync -aAXvh --info=progress2 --info=stats1 --exclude={"/dev/
 alias backup-vps='rsync -aAXzzh --info=progress2 --delete vps-root:/mnt/data/ /run/media/isaac/isaac_backup/server'
 
 # Easy configuration aliases
-alias config-zsh="vim $XDG_CONFIG_HOME/zsh/.zshrc"
-alias config-alias="vim $XDG_CONFIG_HOME/zsh/alias.zsh"
-alias config-env="vim $XDG_CONFIG_HOME/zsh/env.zsh"
-alias config-key="vim $XDG_CONFIG_HOME/zsh/keys.zsh"
-alias config-func="vim $XDG_CONFIG_HOME/zsh/functions.zsh"
-alias config-fzf="vim $XDG_CONFIG_HOME/zsh/fzf.zsh"
+alias c-zsh="vim $XDG_CONFIG_HOME/zsh/.zshrc"
+alias c-alias="vim $XDG_CONFIG_HOME/zsh/alias.zsh"
+alias c-env="vim $XDG_CONFIG_HOME/zsh/env.zsh"
+alias c-key="vim $XDG_CONFIG_HOME/zsh/keys.zsh"
+alias c-func="vim $XDG_CONFIG_HOME/zsh/functions.zsh"
+alias c-fzf="vim $XDG_CONFIG_HOME/zsh/fzf.zsh"
 
 # Make wget follow XDG spec
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
